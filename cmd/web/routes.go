ddfeca0d7c70ed107bc4b864e79009baf828aa62
mnodhe <mnodhe@gmail.com>
2025-03-15 13:19:28 +0330

    add html template and routes

---
+++ cmd/web/routes.go
@@ -0,0 +1,16 @@
+package main
+
+import (
+	"github.com/go-chi/chi/v5"
+	"github.com/go-chi/chi/v5/middleware"
+	"net/http"
+	"time"
+)
+
+func (app *application) routes() http.Handler {
+	mux := chi.NewRouter()
+	mux.Use(middleware.Recoverer)
+	mux.Use(middleware.Timeout(60 * time.Second)) // for nasty people that trying to add
+	mux.Get("/", app.ShowHome)
+	return mux
+}
--- go.mod
+++ go.mod
@@ -1,3 +1,5 @@
 module go-design-pattern-go-find-pet-app
 
 go 1.24
+
+require github.com/go-chi/chi/v5 v5.2.1 // indirect
