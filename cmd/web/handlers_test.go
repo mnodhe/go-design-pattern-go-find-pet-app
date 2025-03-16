d171408670689c5083091c713205b30b6b8425b5
mnodhe <mnodhe@gmail.com>
2025-03-16 13:47:20 +0330

    repository pattern part 3 : created a test but it need db running and also call api it's not prefect test should be running on their own

---
+++ cmd/web/handlers_test.go
@@ -0,0 +1,24 @@
+package main
+
+import (
+	"net/http"
+	"net/http/httptest"
+	"testing"
+)
+
+func TestApplication_GetAllDogBreedsJSON(t *testing.T) {
+	// create a request
+	req, _ := http.NewRequest("GET", "/api/dog-breeds", nil)
+
+	// create a response recorder
+	rr := httptest.NewRecorder()
+
+	// create the handler
+	handler := http.HandlerFunc(testApp.GetAllDogBreedsJSON)
+	// serve handler
+	handler.ServeHTTP(rr, req)
+	// check response
+	if rr.Code != http.StatusOK {
+		t.Errorf("handler returned wrong status code: got %v want %v", rr.Code, http.StatusOK)
+	}
+}
---
+++ cmd/web/setup_test.go
@@ -0,0 +1,23 @@
+package main
+
+import (
+	"go-design-pattern-go-find-pet-app/models"
+	"log"
+	"os"
+	"testing"
+)
+
+var testApp application
+
+func TestMain(m *testing.M) {
+	dsn := "mariadb:myverysecretpassword@tcp(localhost:3306)/breeders?parseTime=true&tls=false&collation=utf8_unicode_ci&timeout=5s"
+	db, err := initMySQLDB(dsn)
+	if err != nil {
+		log.Panic(err)
+	}
+	testApp = application{
+		DB:     db,
+		Models: *models.New(db),
+	}
+	os.Exit(m.Run())
+}
